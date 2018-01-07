3538b720f7a5f654d96bc6ac237228d8822b3301
daiki <dik.piyo.piyo@gmail.com>
2018-01-07 18:04:14 +0900

    finish tutorial

---
+++ docs/js/tutorial.js
@@ -0,0 +1,120 @@
+const chart1 = c3.generate({
+    bindto: '#chart1',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+});
+
+const chart2 = c3.generate({
+    bindto: '#chart2',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2' // ADD
+      }
+    },
+    axis: {
+      y2: {
+        show: true // ADD
+      }
+    }
+});
+
+const chart3 = c3.generate({
+    bindto: '#chart3',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2'
+      }
+    },
+    axis: {
+      y: {
+        label: { // ADD
+          text: 'Y Label',
+          position: 'outer-middle'
+        }
+      },
+      y2: {
+        show: true,
+        label: { // ADD
+          text: 'Y2 Label',
+          position: 'outer-middle'
+        }
+      }
+    }
+});
+
+const chart4 = c3.generate({
+    bindto: '#chart4',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2'
+      },
+      types: {
+        data2: 'bar' // ADD
+      }
+    },
+    axis: {
+      y: {
+        label: {
+          text: 'Y Label',
+          position: 'outer-middle'
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: 'Y2 Label',
+          position: 'outer-middle'
+        }
+      }
+    }
+});
+
+const chart5 = c3.generate({
+    bindto: '#chart5',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ],
+      axes: {
+        data2: 'y2'
+      },
+      types: {
+        data2: 'bar'
+      }
+    },
+    axis: {
+      y: {
+        label: {
+          text: 'Y Label',
+          position: 'outer-middle'
+        },
+        tick: {
+          format: d3.format("$,") // ADD
+        }
+      },
+      y2: {
+        show: true,
+        label: {
+          text: 'Y2 Label',
+          position: 'outer-middle'
+        }
+      }
+    }
+});
